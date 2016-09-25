0124b3782bf08a583f6c70eec586acbacb754b45
viagram <viagram.yang@gmail.com>
2016-09-25 21:10:07 +0800

    20160925

--- adbyby.sh
+++ adbyby.sh
@@ -2,6 +2,11 @@
 # Adbyby
 # By viagram
 # viagram@qq.com
-
-opkg install https://raw.githubusercontent.com/viagram/adbyby/master/adbyby_2.7_ar71xx.ipk --force-depends
-opkg install https://raw.githubusercontent.com/viagram/adbyby/master/luci-app-adbyby_2016-01-27_all.ipk --force-depends
+opkg update
+opkg install curl wget
+curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/adbyby_2.7_ar71xx.ipk -o /root/adbyby_2.7_ar71xx.ipk 
+curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/luci-app-adbyby_2016-01-27_all.ipk -o /root/luci-app-adbyby_2016-01-27_all.ipk
+opkg install adbyby_2.7_ar71xx.ipk --force-depends
+opkg install luci-app-adbyby_2016-01-27_all.ipk --force-depends
+rm -f /root/adbyby_2.7_ar71xx.ipk 
+rm -f /root/luci-app-adbyby_2016-01-27_all.ipk
