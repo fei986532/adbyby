d216aed89b1c1e38e24de5ad306d3563d8a2fe0d
viagram <viagram.yang@gmail.com>
2016-09-30 15:20:45 +0800

    20160930

--- up.sh
+++ up.sh
@@ -3,18 +3,18 @@
 # By viagram
 # 
 
-OnTime="30 6"
+OnTime="30"
 DATA_PATH='/usr/share/adbyby/data'
 UPDDATE_URL='http://update.adbyby.com/rule3'
 
 gettext()
 {
     local url=$1
-	echo "update the "$url
+	echo "规则地址: "$url
 	if [ -f $DATA_PATH/upadtext.tmp ]; then
         rm -f $DATA_PATH/upadtext.tmp
 	fi
-	wget -c $url -O $DATA_PATH/upadtext.tmp
+	wget -c $url -O $DATA_PATH/upadtext.tmp 2>/dev/null
 }
 
 upadtext()
@@ -31,22 +31,24 @@ upadtext()
     
     oldver=`head -1 $DATA_PATH/$parstr.txt | awk -F' ' '{print $3,$4}'`
     newver=`head -1 $DATA_PATH/upadtext.tmp | awk -F' ' '{print $3,$4}'`
-    echo "old: "$oldver
-    echo "new: "$newver
+    echo -e "$parstr规则本地版本: "$oldver
+    echo -e "$parstr规则在线版本: "$newver
     if [ "$oldver" != "$newver" ];then
         \mv $DATA_PATH/upadtext.tmp $DATA_PATH/$parstr.txt
-        echo "update $parstr OK."
+        echo -e "更新 $parstr 规则成功."
     else
-        echo "$parstr no update."
+        echo -e "$parstr 规则没有新版."
     fi
 }
 
 InstallLED(){
     ln -sf /etc/config/adbyby_up /bin/adbyby_up
     [ ! -e '/etc/config/adbyby_up' ] && \mv $0 /etc/config/adbyby_up && chmod 777 /etc/config/adbyby_up
-    [ -z "`grep \"${OnTime} \* \* \* /etc/config/adbyby_up\" /etc/crontabs/root`" ] && echo "${OnTime} * * * /etc/config/adbyby_up" >>/etc/crontabs/root && crontab -l
+    [ -z "`grep \"\*/${OnTime} \* \* \* \* /etc/config/adbyby_up\" /etc/crontabs/root`" ] && echo "*/${OnTime} * * * * /etc/config/adbyby_up" >>/etc/crontabs/root && crontab -l
 }
 
-InstallLED
-upadtext lazy
-upadtext video
+aa=$(InstallLED)
+echo "正在更新过滤规则,请稍等..."
+bb=$(upadtext lazy)
+cc=$(upadtext video)
+echo -e "$aa\n$bb\n\n$cc"
