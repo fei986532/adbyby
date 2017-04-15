7dfe7fbef27212b96e161c4823a2e7cf04590a05
viagram <viagram.yang@gmail.com>
2017-04-15 18:56:30 +0800

    update

--- adbyby.sh
+++ adbyby.sh
@@ -2,8 +2,6 @@
 # Adbyby
 # By viagram
 # viagram@qq.com
-opkg update
-opkg install curl wget
 
 curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/adbyby_2.7_ar71xx.ipk -o /root/adbyby_2.7_ar71xx.ipk
 opkg install adbyby_2.7_ar71xx.ipk --force-depends --force-overwrite --force-maintainer
@@ -12,9 +10,6 @@ rm -f /root/adbyby_2.7_ar71xx.ipk
 curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/luci-app-adbyby_2016-01-27_all.ipk -o /root/luci-app-adbyby_2016-01-27_all.ipk
 opkg install luci-app-adbyby_2016-01-27_all.ipk --force-depends --force-overwrite --force-maintainer
 rm -f /root/luci-app-adbyby_2016-01-27_all.ipk
-
-curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/up.sh -o /root/up.sh
-sh /root/up.sh
 
 curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/up.sh -o /root/up.sh
 sh /root/up.sh
