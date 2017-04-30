bbbbeac778716d284caadf8b77ff49007d8e3985
viagram <viagram.yang@gmail.com>
2017-04-30 22:53:43 +0800

    Add LEDE

---
+++ adbyby-lede.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Adbyby
+# By viagram
+# viagram@qq.com
+
+curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/adbyby_2.7-20160110_mips_24kc.ipk -o /tmp/adbyby_2.7-20160110_mips_24kc.ipk
+opkg install adbyby_2.7-20160110_mips_24kc.ipk --force-depends --force-overwrite --force-maintainer
+rm -f /tmp/adbyby_2.7-20160110_mips_24kc.ipk
+
+curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/luci-app-adbyby_git-16.324.51057-1c27f6b-1_all.ipk -o /tmp/luci-app-adbyby_git-16.324.51057-1c27f6b-1_all.ipk
+opkg install luci-app-adbyby_git-16.324.51057-1c27f6b-1_all.ipk --force-depends --force-overwrite --force-maintainer
+rm -f /tmp/luci-app-adbyby_git-16.324.51057-1c27f6b-1_all.ipk
+
+curl -sk https://raw.githubusercontent.com/viagram/adbyby/master/up.sh -o /tmp/up.sh
+sh /tmp/up.sh
