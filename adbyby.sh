e994d185eceb06816a4dd990041a91896a5d4a1b
viagram <viagram.yang@gmail.com>
2017-06-06 18:35:54 +0800

    更新脚本

--- adbyby.sh
+++ adbyby.sh
@@ -6,11 +6,11 @@
 opkg update
 
 wget -c https://raw.githubusercontent.com/viagram/adbyby/master/adbyby_2.7.ipk -O /tmp/adbyby_2.7.ipk  --no-check-certificate
-opkg install /tmp/adbyby_2.7.ipk #--force-depends --force-Overwrite --force-maintainer
+opkg install /tmp/adbyby_2.7.ipk --force-depends #--force-Overwrite --force-maintainer
 rm -f /tmp/adbyby_2.7.ipk
 
 wget -c https://raw.githubusercontent.com/viagram/adbyby/master/luci-app-adbyby.ipk -O /tmp/luci-app-adbyby.ipk  --no-check-certificate
-opkg install /tmp/luci-app-adbyby.ipk #--force-depends --force-Overwrite --force-maintainer
+opkg install /tmp/luci-app-adbyby.ipk --force-depends #--force-Overwrite --force-maintainer
 rm -f /tmp/luci-app-adbyby.ipk
 
 wget -c https://raw.githubusercontent.com/viagram/adbyby/master/up.sh -O /tmp/up.sh --no-check-certificate
